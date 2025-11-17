a3d609d2f1ffe97a87058d2683762139939bb69b
bojanpremcheski <bojan.premcheski@gmail.com>
2025-11-17 21:36:44 +0100

    add routes

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get("/", (req, res) => {
   });   
 });
 
+app.get("/1", (req, res) => {
+  res.json({
+    Id
+  });   
+});
+
+app.get("/2", (req, res) => {
+  res.json({
+     pn
+  });   
+});
+
+app.get("/3", (req, res) => {
+  res.json({
+    PN: "PN",
+  });   
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening at <http://localhost>:${port}`);
