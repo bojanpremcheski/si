0ba0a212d22607c1f5903d115d1c69e464086d97
bojanpremcheski <bojan.premcheski@gmail.com>
2025-11-17 21:16:33 +0100

    5 commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get("/", (req, res) => {
+  res.json({
+    Id: "Id",
+  });   
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening at <http://localhost>:${port}`);
+});
